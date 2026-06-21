aec4376bc7ca2e687170401e77b5d46e1b0f47b4
mason hall <hall.mason.a@proton.me>
2026-06-20 20:08:40 -0400

    shorten license statement

--- src/execution/insert.rs
+++ src/execution/insert.rs
@@ -1,3 +1,7 @@
+/* Copyright (C) 2026 Mason Hall.
+ *
+ * GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
 use crate::{
     buffer_pool::BufferPool,
     database::Database,
